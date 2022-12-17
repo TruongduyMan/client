420ddda4a473bd49ce435c20dcdb30f3f454f77a
TruongduyMan <64964496+TruongduyMan@users.noreply.github.com>
2022-12-17 21:06:18 +0700

    Commit5

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,64 @@
 import React from "react";
+import "./Footer.scss"
 
 const Footer = () =>{
     return(
-        <div>Footer</div>
+        <div className="footer">
+            <div className="top">
+                
+                <div className="item">
+                    <h1>Category</h1>
+                    <span>Women</span>
+                    <span>Men</span>
+                    <span>Shoes</span>
+                    <span>Accessory</span>
+                    <span>New Arrivals</span>
+
+                </div>
+                
+                <div className="item">
+                    <h1>Links</h1>
+                    <span>FAQ</span>
+                    <span>Pages</span>
+                    <span>Stores</span>
+                    <span>Compare</span>
+                    <span>Cokkies</span>
+                    
+                </div>
+
+                <div className="item">
+                    <h1>About</h1>
+                    <span>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Omnis nobis quo perferendis animi soluta tempore nostrum earum 
+                    odit neque qui maiores corporis reiciendis accusamus, 
+                    quaerat quae recusandae corrupti, exercitationem nisi?
+                    </span>
+
+                </div>
+
+                <div className="item">
+                    <h1>Contact</h1>
+                    <span>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Omnis nobis quo perferendis animi soluta tempore nostrum earum 
+                        odit neque qui maiores corporis reiciendis accusamus, 
+                        quaerat quae recusandae corrupti, exercitationem nisi?
+                    </span>
+                    
+                </div>
+            </div>
+
+            <div className="bottom">
+                <div className="left">
+                    <span className="logo">CAT SOTRE</span>
+                    <sapn className="copyright">© Copyright 2023. All Rights Reserved</sapn>
+                </div>
+                <div className="right">
+                    <img src="/img/payment.png" alt=""></img>
+                </div>
+            </div>
+        </div>
     )
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,18 +4,69 @@ import SearchIcon from '@mui/icons-material/Search';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Link } from "react-router-dom";
+import "./Navbar.scss"
+
 const Navbar = () =>{
     return(
         <div className="navbar">
             <div className="wrapper">
                 <div className="left">
+                    
                     <div className="item">
-                        <img src="/img/vietnam_640.png" alt=""></img>
+                        <img src="/img/en.png" alt=""></img>
                         <KeyboardArrowDownIcon/>
                     </div>
+                    <div className="item">
+                        <span>USD</span>
+                        <KeyboardArrowDownIcon/>
+                    </div>
+                    
+                    <div className="item">
+                        <Link className="link" to="/products/1">Women</Link>
+                    </div>
+                    
+                    <div className="item">
+                        <Link className="link" to="/products/2">Men</Link>
+                    </div>
+                    
+                    <div className="item">
+                        <Link className="link" to="/products/3">Chidren</Link>
+                    </div>
+                    
                 </div>
-                <div className="center"></div>
-                <div className="right"></div>    
+                
+                <div className="center">
+                    <Link className="link" to="/">CAT STORE</Link>
+                </div>
+
+                <div className="right">
+                    <div className="item">
+                        <Link className="link" to="/">Home Page</Link>
+                    </div>
+                    
+                    <div className="item">
+                        <Link className="link" to="/">About</Link>
+                    </div>
+                    
+                    <div className="item">
+                        <Link className="link" to="/">Contact</Link>
+                    </div>
+
+                    <div className="item">
+                        <Link className="link" to="/">Stores</Link>
+                    </div>
+                    <div className="icons">
+                        <SearchIcon/>
+                        <PersonOutlineIcon/>
+                        <FavoriteBorderIcon/>
+                        <div className="cardIcon">
+                            <ShoppingCartIcon/>
+                            <span>0</span>
+                        </div>
+                        
+                    </div>
+                </div>    
             </div>
         </div>
     )
